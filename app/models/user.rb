dbcde58904152a2acd7aae034d36b134dd988ac8
mantasavas@gmail.com <mantasavas@gmail.com>
2018-04-04 17:16:58 +0300

    Fixed code style: changed hash syntax, used single quotes there necessary, fixed spacing etc, solved merge issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: :google_oauth2
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
     # binding.pry
